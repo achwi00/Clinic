1b4e34eb55f834e4d04177ee25f5afe9a11bebba
achwi00 <achwierut00@gmail.com>
2024-01-06 11:37:21 +0100

    Fixed html document displaying, mapped / and /register

--- Clinic/src/main/java/com/project/controllers/LoginController.java
+++ Clinic/src/main/java/com/project/controllers/LoginController.java
@@ -1,5 +1,13 @@
 package com.project.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LoginController
 {
+    @RequestMapping("/")
+    public String showLogin(){
+        return "index.html";
+    }
 }
